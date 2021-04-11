ea5bed8c176f648e3fe4d450885c7d73ba94a9b8
Anastasia M <anastasiamaschenko103@gmail.com>
2021-04-11 15:32:19 +0300

    Find by manufacture

--- app.rb
+++ app.rb
@@ -1,22 +1,31 @@
 require 'mongo'
 
-class MongoOperations
+class DB
   def initialize()
     @client = Mongo::Client.new(["#{ENV['MONGO_HOST']}:#{ENV['MONGO_PORT']}"], :database => 'cars')
     @collection = @client[:cars]
   end
+
+  def find_by_manufacture(manufacture)
+    @collection.find({manufacture: manufacture})
+  end
+
+  def self.print_collection(collection)
+    collection.each {|record| puts record}
+  end
 end
 
 def handle_input(input)
+  db = DB.new()
   case input
-  in ["manufacture", manufacture]
-    "Here will be manufacture #{manufacture} output"
-  in ["later_then", year]
+  in ['manufacture', manufacture]
+    DB.print_collection(db.find_by_manufacture(manufacture))
+  in ['later_then', year]
     "Here will be later then #{year} output"
-  in ["options", id]
+  in ['options', id]
     "Here will be options #{id} output"
-  in ["help"]
-    """
+  in ['help']
+    puts """
     Flags for app.rb:
     manufacture <manufacture> -- finds all cars by manufacture
     later_then <year> -- finds all cars which were produced after year
@@ -24,8 +33,8 @@ def handle_input(input)
     help -- puts help
     """
   else
-    "Wrong command. Print \"ruby app.rb help\" for help."
+    puts 'Wrong command. Print "ruby app.rb help" for help.'
   end
 end
 
-puts handle_input(ARGV)
+handle_input(ARGV)
